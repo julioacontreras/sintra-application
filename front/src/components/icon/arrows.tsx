12e28a8c9dd9986aed750c2fb8e82e80e852a904
JULIO <julio.aranis.contreras@gmail.com>
2023-04-09 11:49:43 +0200

    feat(editor): add nodes in editor
    add nodes in editor

--- front/src/components/icon/arrows.tsx
+++ front/src/components/icon/arrows.tsx
@@ -7,9 +7,9 @@ export type IconProps = {
 
 export default function IconArrows(props: IconProps) {
     return <g fill="none" transform={`translate(${props.x} ${props.y})`}>
-        <path d="M11.1389 7.35641L12.6645 5.83087L11.1416 4.30803" stroke={ props.stroke } stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M1.00003 5.83356L5.44235 5.83357" stroke={ props.stroke } stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M8.23292 5.83628L12.6672 5.83358" stroke={ props.stroke } stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M2.52554 4.30804L0.999996 5.83359L2.52554 7.35913" stroke={ props.stroke } stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M11.1389 7.35641L12.6645 5.83087L11.1416 4.30803" stroke={ props.stroke } strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M1.00003 5.83356L5.44235 5.83357" stroke={ props.stroke } strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M8.23292 5.83628L12.6672 5.83358" stroke={ props.stroke } strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M2.52554 4.30804L0.999996 5.83359L2.52554 7.35913" stroke={ props.stroke } strokeLinecap="round" strokeLinejoin="round"/>
       </g>
 }
